d92cddd70c1e40e26453713896948d5620398c8a
SCLIFFORD78 <faugh01@gmail.com>
2020-11-18 01:10:12 +0000

    Am now subscribing to mqtt data and saving to MySQL database

---
+++ windows/heating/heating.sql
@@ -0,0 +1,130 @@
+-- -----------------------------------------------------
+-- Drop the 'mqtt' database/schema
+-- -----------------------------------------------------
+
+DROP SCHEMA IF EXISTS mqtt;
+-- -----------------------------------------------------
+-- Create 'inventory' database/schema and use this database
+-- -----------------------------------------------------
+
+CREATE SCHEMA IF NOT EXISTS mqtt;
+
+USE mqtt;
+
+-- -----------------------------------------------------
+-- Drop tables
+-- -----------------------------------------------------
+
+-- drop table data_values;
+
+-- -----------------------------------------------------
+-- Create table data_values
+-- -----------------------------------------------------
+
+CREATE TABLE flueGas
+(
+    ID          INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    flueGas     FLOAT,
+    `timeStamp` VARCHAR(255)
+);
+
+CREATE TABLE boilerTemp
+(
+    ID          INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    boilerTemp  FLOAT,
+    `timeStamp` VARCHAR(255)
+);
+
+CREATE TABLE bufferTop
+(
+    ID          INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    bufferTop   FLOAT,
+    `timeStamp` VARCHAR(255)
+);
+
+CREATE TABLE bufferMid
+(
+    ID          INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    bufferMid   FLOAT,
+    `timeStamp` VARCHAR(255)
+);
+
+CREATE TABLE hotWater
+(
+    ID          INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    hotWater    FLOAT,
+    `timeStamp` VARCHAR(255)
+);
+
+CREATE TABLE bufferBottom
+(
+	ID          INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    bufferBottom FLOAT,
+    `timeStamp`  VARCHAR(255)
+);
+
+CREATE TABLE heartBeat
+(
+    ID          INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    heartBeat   INT,
+    `timeStamp` VARCHAR(255)
+);
+
+CREATE TABLE woodFan
+(
+	ID          INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    woodFan     INT,
+    `timeStamp` VARCHAR(255)
+);
+
+
+CREATE TABLE woodCircPump
+(
+    ID           INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    woodCircPump INT,
+    `timeStamp`  VARCHAR(255)
+);
+
+CREATE TABLE woodHeatCircPump
+(
+    ID               INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    woodHeatCircPump INT,
+    `timeStamp`      VARCHAR(255)
+);
+
+CREATE TABLE oilBoiler
+(
+	ID          INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    oilBoiler   INT,
+    `timeStamp` VARCHAR(255)
+);
+
+CREATE TABLE hotWaterValve
+(
+    ID            INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    hotWaterValve INT,
+    `timeStamp`   VARCHAR(255)
+);
+
+CREATE TABLE switchOver
+(
+	ID          INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    switchOver  INT,
+    `timeStamp` VARCHAR(255)
+);
+
+
+
+CREATE TABLE startButton
+(
+    ID          INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    startButton INT,
+    `timeStamp` VARCHAR(255)
+);
+
+SELECT
+ from_unixtime(timestamp, '%D %m %Y %H:%i:%s') AS Time
+FROM 
+mqtt.startbutton;
+
+-- --------------------------------------------
---
+++ windows/heating/sub.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python3
+
+import paho.mqtt.client as mqtt
+from urllib.parse import urlparse
+import sys
+import mysql.connector
+import json
+import time
+
+try:
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="Ranger01?",
+        database="mqtt"
+    )
+except mysql.connector.Error as err:
+    print("Something went wrong: {}".format(err))
+
+# Define event callbacks
+def on_connect(client, userdata, flags, rc):
+    print("Connection Result: " + str(rc))
+
+def on_message(client, obj, msg):
+    # Prepare Data, separate columns and values
+    m_decode=str(msg.payload.decode("utf-8","ignore"))
+    print("message received",m_decode)
+    m_in=json.loads(m_decode)
+    #columns = ', '.join(m_in.keys())+""
+    column1 = list(m_in.keys())[0]
+    column2 = list(m_in.keys())[1]
+    value1 = list(m_in.values())[0]
+    value2 = list(m_in.values())[1]
+    mycursor = mydb.cursor()
+    createTable = "CREATE TABLE IF NOT EXISTS %s(ID INT AUTO_INCREMENT NOT NULL PRIMARY KEY,%s INT,%s VARCHAR(255))"%(column1,column1,column2)
+    sql = "INSERT INTO %s (%s,%s) VALUES (%s,%s)" %(column1,column1,column2,value1,value2)
+    mycursor.execute(createTable)
+    mycursor.execute(sql)
+
+
+    mydb.commit()
+    print(mycursor.rowcount, "record inserted.")
+
+
+def on_subscribe(client, obj, mid, granted_qos):
+    print("Subscribed,  QOS granted: "+ str(granted_qos))
+
+mqttc = mqtt.Client()
+
+# Assign event callbacks
+mqttc.on_message = on_message
+mqttc.on_connect = on_connect
+mqttc.on_subscribe = on_subscribe
+
+# parse mqtt url for connection details
+#url_str = sys.argv[1]
+url_str = "mqtt://broker.hivemq.com:1883/Heating/home"
+url = urlparse(url_str)
+base_topic = url.path[1:]
+
+# Connect
+if (url.username):
+    mqttc.username_pw_set(url.username, url.password)
+print(url.hostname)
+print(url.port)
+mqttc.connect(url.hostname, url.port)
+
+# Start subscribe, with QoS level 0
+mqttc.subscribe(base_topic+"/#", 2)
+mqttc.loop_forever()
+
+# Continue the network loop, exit when an error occurs
+rc = 0
+while rc == 0:
+    rc = mqttc.loop()
+
+print("rc: " + str(rc))
